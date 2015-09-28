337d04723c7f06fecbcccb566052afa5ef201399
Ricard Marxer <email@ricardmarxer.com>
2015-09-28 13:43:22 +0100

    change the recognition path

--- egs/uaspeech/s5/path.sh
+++ egs/uaspeech/s5/path.sh
@@ -5,10 +5,10 @@ fullhost=`hostname -f`
 export KALDI_ROOT=`pwd`/../../..
 if [ ${fullhost#*.}=="windows.dcs.shef.ac.uk" ]; then
     export UASPEECH_ROOT="$HOME/data/UASPEECH/"
-    export REC_ROOT="."
+    export REC_ROOT="results"
 elif [ ${fullhost#*.}=="iceberg.shef.ac.uk" ]; then
     export UASPEECH_ROOT="/fastdata/ac1rpm/UASPEECH/"
-    export REC_ROOT="."
+    export REC_ROOT="results"
 fi
 
 export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH:$KALDI_ROOT/tools/sph2pipe_v2.5
