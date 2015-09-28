aaa7f82cacfb462dbcd7d6490a77a1a1f53dc34e
Ricard Marxer <email@ricardmarxer.com>
2015-09-28 12:29:27 +0100

    more work on the uaspeech example

--- egs/uaspeech/s5/run.sh
+++ egs/uaspeech/s5/run.sh
@@ -68,9 +68,42 @@ fi
 
 # Decode monophone model
 if [ ${stage} -le 3 ]; then
-  utils/mkgraph.sh data/lang_largevocab_test exp/tri1 exp/tri1/graph_largevocab || exit 1
+  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${REC_ROOT}/exp/tri1 ${REC_ROOT}/exp/tri1/graph_largevocab || exit 1
 
-  steps/decode.sh --nj ${decode_nj} --cmd "$decode_cmd" \
+  steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
     --num-threads 4 --parallel-opts "${parallel_opts}" \
-    exp/tri1/graph_largevocab data/test exp/tri1/decode_largevocab_test || exit 1
+    ${REC_ROOT}/exp/tri1/graph_largevocab ${REC_ROOT}/data/test ${REC_ROOT}/exp/tri1/decode_largevocab_test || exit 1
+fi
+
+
+# Train triphone models
+if [ ${stage} -le 2 ]; then
+  steps/align_si.sh --nj $nj \
+    ${REC_ROOT}/data/train ${REC_ROOT}/data/lang ${REC_ROOT}/exp/tri1 ${REC_ROOT}/exp/tri1_ali || exit 1;
+
+  steps/train_lda_mllt.sh \
+    --splice-opts "--left-context=3 --right-context=3" \
+    2500 15000 ${REC_ROOT}/data/train ${REC_ROOT}/data/lang ${REC_ROOT}/exp/tri1_ali ${REC_ROOT}/exp/tri2b || exit 1;
+
+  steps/align_si.sh  --nj $nj \
+    --use-graphs true ${REC_ROOT}/data/train ${REC_ROOT}/data/lang ${REC_ROOT}/exp/tri2b ${REC_ROOT}/exp/tri2b_ali  || exit 1;
+
+  steps/train_sat.sh \
+    2500 15000 ${REC_ROOT}/data/train ${REC_ROOT}/data/lang ${REC_ROOT}/exp/tri2b_ali ${REC_ROOT}/exp/tri3b || exit 1;
+fi
+
+
+# Decode triphone models
+if [ ${stage} -le 3 ]; then
+  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${REC_ROOT}/exp/tri3b ${REC_ROOT}/exp/tri3b/graph_largevocab || exit 1;
+
+  steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
+    --num-threads 4 --parallel-opts "${parallel_opts}" \
+    ${REC_ROOT}/exp/tri3b/graph_largevocab ${REC_ROOT}/data/test ${REC_ROOT}/exp/tri3b/decode_largevocab_test || exit 1
+
+  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${REC_ROOT}/exp/tri3b ${REC_ROOT}/exp/tri3b/graph_largevocab || exit 1;
+
+  steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
+    --num-threads 4 --parallel-opts "${parallel_opts}" \
+    ${REC_ROOT}/exp/tri3b/graph_largevocab ${REC_ROOT}/data/test ${REC_ROOT}/exp/tri3b/decode_largevocab_test || exit 1
 fi
