fdfa9c53ea282f4653e707df6f53942723abb5c0
Ricard Marxer <email@ricardmarxer.com>
2015-09-29 11:38:26 +0100

    create a wav_passhtrough script

---
+++ egs/uaspeech/s5/local/wav_passthrough.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""wav_passthrough.py
+
+Reads (parses) and rewrites the WAV file to try to solve any corruptions
+that may cause Kaldi to not read it correctly
+
+Usage:
+wav_passthrough.py <input_wav> <output_wav>
+wav_passthrough.py --help
+
+Options:
+<input_wav>             path to the input wav file (use - for stdin)
+<output_wav>            path to the output wav file (use - for stdout)
+--help, -h              display this help screen
+"""
+
+from __future__ import print_function, unicode_literals
+import wave
+import docopt
+import sys
+
+
+def main():
+    args = docopt.docopt(__doc__)
+
+    try:
+        if args['<input_wav>'] == '-':
+            f = sys.stdin
+        else:
+            f = open(args['<input_wav>'], 'rb')
+
+        if args['<output_wav>'] == '-':
+            fo = sys.stdout
+        else:
+            fo = open(args['<output_wav>'], 'wb')
+
+        w = wave.open(f, 'r')
+        wo = wave.open(fo, 'wb')
+
+        wo.setparams(w.getparams())
+        nframes = w.getnframes()
+        wo.setnframes(nframes)
+        wo.writeframes(w.readframes(nframes))
+
+    finally:
+        w.close()
+        wo.close()
+
+        f.close()
+        fo.close()
+
+
+if __name__ == '__main__':
+    main()
