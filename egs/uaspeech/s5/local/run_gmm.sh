8a7f3ddb6d0d5559c4e2879f3219d6e53ada7c4d
Ricard Marxer <email@ricardmarxer.com>
2015-10-05 16:43:48 +0100

    make the run_gmm do the adaptation as well

--- egs/uaspeech/s5/local/run_gmm.sh
+++ egs/uaspeech/s5/local/run_gmm.sh
@@ -16,6 +16,7 @@ script_path=`dirname $0`
 . ${script_path}/../path.sh
 
 nj=12
+align_nj=1
 decode_nj=1
 parallel_opts=
 
@@ -36,27 +37,27 @@ dir=$1
 # Train monophone model
 if [ ${stage} -le 2 ]; then
   steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
-    ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a || exit 1
+    ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a || exit 1;
 
   steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-    ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a ${dir}/exp/mono0a_ali || exit 1
+    ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a ${dir}/exp/mono0a_ali || exit 1;
 
   steps/train_deltas.sh --cmd "$train_cmd" \
-    2500 30000 ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a_ali ${dir}/exp/tri1 || exit 1
+    2500 30000 ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/mono0a_ali ${dir}/exp/tri1 || exit 1;
 fi
 
 # Decode monophone model
 if [ ${stage} -le 3 ]; then
-  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri1 ${dir}/exp/tri1/graph_largevocab || exit 1
+  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri1 ${dir}/exp/tri1/graph_largevocab || exit 1;
 
-  steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
+  steps/decode.sh --nj ${decode_nj} --cmd "$decode_cmd" \
     --num-threads 4 --parallel-opts "${parallel_opts}" \
-    --scoring-opts "--min-lmwt 0 --max-lmwt 0" \
-    ${dir}/exp/tri1/graph_largevocab ${dir}/test ${dir}/exp/tri1/decode_largevocab_test || exit 1
+    --scoring-opts "--min-lmwt 1 --max-lmwt 1" \
+    ${dir}/exp/tri1/graph_largevocab ${dir}/test ${dir}/exp/tri1/decode_largevocab_test || exit 1;
 fi
 
 # Train triphone models
-if [ ${stage} -le 2 ]; then
+if [ ${stage} -le 4 ]; then
   steps/align_si.sh --nj $nj \
     ${dir}/train ${REC_ROOT}/data/lang ${dir}/exp/tri1 ${dir}/exp/tri1_ali || exit 1;
 
@@ -72,18 +73,38 @@ if [ ${stage} -le 2 ]; then
 fi
 
 # Decode triphone models
-if [ ${stage} -le 3 ]; then
+if [ ${stage} -le 5 ]; then
+  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri2b ${dir}/exp/tri2b/graph_largevocab || exit 1;
+
+  steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
+    --num-threads 4 --parallel-opts "${parallel_opts}" \
+    --scoring-opts "--min-lmwt 1 --max-lmwt 1" \
+    ${dir}/exp/tri2b/graph_largevocab ${dir}/test ${dir}/exp/tri2b/decode_largevocab_test || exit 1;
+
   utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri3b ${dir}/exp/tri3b/graph_largevocab || exit 1;
 
   steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
     --num-threads 4 --parallel-opts "${parallel_opts}" \
-    --scoring-opts "--min-lmwt 0 --max-lmwt 0" \
-    ${dir}/exp/tri3b/graph_largevocab ${dir}/test ${dir}/exp/tri3b/decode_largevocab_test || exit 1
+    --scoring-opts "--min-lmwt 1 --max-lmwt 1" \
+    ${dir}/exp/tri3b/graph_largevocab ${dir}/test ${dir}/exp/tri3b/decode_largevocab_test || exit 1;
+fi
 
-  utils/mkgraph.sh ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri3b ${dir}/exp/tri3b/graph_largevocab || exit 1;
+# Adapt using the 'adapt' data dir
+if [ ${stage} -le 6 ]; then
+  steps/align_si.sh  --nj ${align_nj} --cmd "$train_cmd" \
+    ${dir}/adapt ${REC_ROOT}/data/lang ${dir}/exp/tri3b ${dir}/exp/tri3b_ali_adapt || exit 1;
+
+  steps/train_map.sh --cmd "$train_cmd" \
+    ${dir}/adapt ${REC_ROOT}/data/lang ${dir}/exp/tri3b_ali_adapt ${dir}/exp/tri3b_adapt || exit 1;
+fi
+
+# Decode the adapted models
+if [ ${stage} -le 7 ]; then
+  utils/mkgraph.sh \
+    ${REC_ROOT}/data/lang_largevocab_test ${dir}/exp/tri3b_adapt ${dir}/exp/tri3b_adapt/graph_largevocab || exit 1;
 
   steps/decode_fmllr.sh --nj ${decode_nj} --cmd "$decode_cmd" \
     --num-threads 4 --parallel-opts "${parallel_opts}" \
-    --scoring-opts "--min-lmwt 0 --max-lmwt 0" \
-    ${dir}/exp/tri3b/graph_largevocab ${dir}/test ${dir}/exp/tri3b/decode_largevocab_test || exit 1
+    --scoring-opts "--min-lmwt 1 --max-lmwt 1" \
+    ${dir}/exp/tri3b_adapt/graph_largevocab ${dir}/test ${dir}/exp/tri3b_adapt/decode_largevocab_test || exit 1;
 fi
