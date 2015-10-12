9467741a1a4ab38ba32c90876fc7dbc2b49fe3aa
Ricard Marxer <email@ricardmarxer.com>
2015-10-12 17:53:37 +0100

    create a show_alignment script

--- egs/uaspeech/s5/local/show_alignment.py
+++ egs/uaspeech/s5/local/show_alignment.py
@@ -3,46 +3,61 @@
 """show_alignment.py
 
 Shows the alignment result from a alignment ark file produced by Kaldi.
+
+Uses the following command to get the alignment:
+ali-to-phones --ctm-output my.mdl ark,t:"gunzip -c ali.1.gz|" - | utils/int2sym.pl -f 5 phones.txt -
+
+Usage:
+show_alignment.py [options] <alignment_file> <lang_directory> [<utterance_id>]
+show_alignment.py --help
+
+Options:
+<alignment_file>          path to alignment file
+<lang_directory>          path to alignment file
+<utternace_id>            file containing the possible words of the language
+--help, -h                display this help screen
 """
 
+
 from __future__ import print_function, unicode_literals
+import os
 import random
+import subprocess
 import scipy
 import scipy.io.wavfile as wavefile
 import matplotlib.pyplot as pplot
-import sys
+import docopt
 
 __author__ = 'rmarxer'
 
 
-def run_length_encoding(x):
-    pos, = scipy.where(scipy.diff(x) != 0)
-    pos = scipy.concatenate(([0], pos+1, [len(x)]))
-    rle = [(a, b, x[a]) for (a, b) in zip(pos[:-1], pos[1:])]
-    return rle
+def load_alignments(alignment_filename, lang_directory):
+    print('Loading {} ...'.format(alignment_filename))
+
+    model_filename = os.path.join(os.path.dirname(alignment_filename), 'final.mdl')
+
+    lines = subprocess.check_output('ali-to-phones --print-args=false --ctm-output {} ark,t:"gunzip -c {}|" - '
+                                    '| utils/int2sym.pl -f 5 {} -'.format(model_filename,
+                                                                          alignment_filename,
+                                                                          os.path.join(lang_directory,
+                                                                                       'phones.txt')),
+                                    stderr=None,
+                                    shell=True)
+
+    # CTM format e.g. F02_B1_C11_M2 1 0.00 4.53 SIL
+    alignments = {}
+    for l in lines.split('\n'):
+        if not l.strip():
+            continue
+
+        utt_id, _, start, length, label = l.strip().split(' ')
+
+        alignments.setdefault(utt_id, []).append((float(start), float(start)+float(length), label))
+
+    return alignments
 
 
-def str_to_mask(strng):
-    return scipy.array(map(int, strng.replace('[', '').replace(']', '').split()))
-
-
-def load_alignment(vad_filename):
-    print('Loading {} ...'.format(vad_filename))
-    vad = {}
-    with open(vad_filename) as f:
-        lines = f.readlines()
-        for l in lines:
-            utt_id, mask = l.strip().split(' ', 1)
-
-            if not utt_id.endswith('2'):
-                continue
-
-            vad[utt_id] = mask
-
-    return vad
-
-
-def show_alignment(utt_id, vad):
+def show_alignment(utt_id, alignments):
     print('Showing {} ...'.format(utt_id))
     speaker, block, word, mic = utt_id.split('_')
 
@@ -52,42 +67,35 @@ def show_alignment(utt_id, vad):
 
     print('Loading WAV {} ...'.format(wav_filename))
     rate, wav = wavefile.read(wav_filename)
-    mask = str_to_mask(vad[utt_id])
-
-    mask_rate = 1000./10.
 
     t = scipy.arange(len(wav)) / float(rate)
-    t_mask = scipy.arange(len(mask)) / float(mask_rate)
 
     pplot.plot(t, wav)
-
-    runs = run_length_encoding(mask)
-
-    for start, end, value in runs:
-        if not value == 1:
+    segments = alignments[utt_id]
+    print(segments)
+    for start, end, label in segments:
+        if label == 'SIL':
             continue
 
-        pplot.axvspan(t_mask[start], t_mask[end], color='g', alpha=0.5)
+        length = end-start
+        pplot.axvspan(start, end, color='g', alpha=0.5)
+        pplot.annotate(label.replace('_B', '').replace('_E', '').replace('_I', '').replace('_S', ''),
+                       xy=(start + length/2.0, 1), xytext=(start + length/2.0, 1.5), textcoords='data')
 
     pplot.show()
 
 
 def main():
-    vad_filename = 'vad.ark'
+    args = docopt.docopt(__doc__)
+
     utt_id = None
 
-    if len(sys.argv) > 1:
-        vad_filename = sys.argv[1]
-
-    if len(sys.argv) > 2:
-        utt_id = sys.argv[2]
-
-    vad = load_alignment(vad_filename)
+    alignments = load_alignments(args['<alignment_file>'], args['<lang_directory>'])
 
     if not utt_id:
-        utt_id = vad.keys()[int(random.random()*len(vad))]
+        utt_id = alignments.keys()[int(random.random()*len(alignments))]
 
-    show_alignment(utt_id, vad)
+    show_alignment(utt_id, alignments)
 
 
 if __name__ == '__main__':
