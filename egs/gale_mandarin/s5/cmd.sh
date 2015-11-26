de6683262ac261e0d2ab31055ca9699954c711a8
Ricard Marxer <r.marxer@sheffield.ac.uk>
2015-11-26 17:25:34 +0000

    make a better cmd.sh add a script to launch long jobs in the grid

--- egs/gale_mandarin/s5/cmd.sh
+++ egs/gale_mandarin/s5/cmd.sh
@@ -5,7 +5,33 @@
 # (make sure your --num-jobs options are no more than
 # the number of cpus on your machine.
 
-export train_cmd="queue.pl -l 'arch=*64*'"
-export decode_cmd="queue.pl -l 'arch=*64*'"
-export cuda_cmd="queue.pl -l gpu=1"
+# Run locally:
+export train_cmd="run.pl"
+export decode_cmd="run.pl"
+export cuda_cmd="run.pl"
+export parallel_opts=""
+export jq_cmd="jq"
 
+fullhost=`hostname -f`
+
+# If on the Iceberg cluster...
+if [[ ${fullhost} == *"iceberg.shef.ac.uk" ]]; then
+  module load apps/python/conda
+  module load compilers/gcc/4.9.2
+  
+  source activate anaconda2-2.3.0
+
+  export train_cmd="queue.pl"
+  export decode_cmd="queue.pl"
+  export cuda_cmd="queue.pl --gpu=1"
+  export parallel_opts="-pe openmp 4"
+  export jq_cmd="local/jq-linux64"
+
+elif [[ ${fullhost} == *"honeydukes.windows.dcs.shef.ac.uk" ]]; then
+  export train_cmd="run.pl"
+  export decode_cmd="run.pl"
+  export cuda_cmd="run.pl"
+  export parallel_opts=""
+  export jq_cmd="local/jq-osx-amd64"
+
+fi
---
+++ egs/gale_mandarin/s5/qsub_long.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for scriptname in "$@"
+do
+    filename=$(basename "$scriptname")
+    filename="${filename%.*}"
+
+    logfile=~/log_uaspeech_${filename}.txt
+
+    qsub -l mem=24G,rmem=20G,h_rt=48:00:00 -j y -o ${logfile} ${scriptname}
+done
+
